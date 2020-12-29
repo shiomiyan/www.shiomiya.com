39358add47de07cb912e7b500ff49c27e9f85f6a
shiomiyan <dev.umisk@gmail.com>
2020-12-29 22:05:55 +0900

    stop using js

--- assets/scripts/navicon-shift.ts
+++ assets/scripts/navicon-shift.ts
@@ -1,10 +1,10 @@
-const 
+const
     nameElement = document.getElementById('nav-name'),
     parentElement = document.getElementById('nav-main');
 
 let incremented = 0;
 function shiftNavIcons() {
-    
+
     let headingElement = document.getElementById('nav-heading');
     if(nameElement.offsetWidth < (headingElement.offsetWidth+10)) {
         parentElement.classList.add('nav-shifted');
@@ -14,9 +14,9 @@ function shiftNavIcons() {
             parentElement.className = parentElement.className.replace(/\bnav-shifted\b/g, '');
             incremented = 0;
         }
-        
+
     }
-    
+
 };
 window.addEventListener('resize', (event) => {
     shiftNavIcons();
